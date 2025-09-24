9e282e673a7f7236c3b3e0f24b9b122219a5ca08
JakeSalter-Coder <jsalty15@gmail.com>
2025-09-24 11:20:48 -0400

    Added a simple cli parser. Added glob cargo package. Updated gitignore for common VSCode files.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use clap::{Parser, ValueHint};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Optional verbose flag
+    #[arg(short, long)]
+    verbose: bool,
+
+    /// Target file(s)
+    #[arg(value_hint = ValueHint::FilePath, required = true)]
+    targets: Vec<String>,
+
+    /// Output file type (positional)
+    output: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    if cli.verbose {
+        println!("Verbose mode is ON");
+    }
+
+    println!("Target files: {:?}", cli.targets);
+    println!("Output type: {}", cli.output);
 }
