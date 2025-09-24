b926928bc14e3218319f11f169c3bb1a3a9d49ab
JakeSalter-Coder <jsalty15@gmail.com>
2025-09-24 11:42:21 -0400

    Added the other flag options.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ use clap::{Parser, ValueHint};
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
+    /// Optional output filename flag
+    #[arg(short, long)]
+    output: String,
+
+    /// Optional quality flag
+    #[arg(short, long, value_parser = clap::value_parser!(i8).range(..=100))]
+    quality: i8,
+
     /// Optional verbose flag
     #[arg(short, long)]
     verbose: bool,
@@ -12,16 +20,18 @@ struct Cli {
     targets: Vec<String>,
 
     /// Output file type (positional)
-    output: String,
+    new: String,
 }
 
 fn main() {
     let cli = Cli::parse();
 
+    
+    println!("Output name: {}", cli.output);
+    println!("Target quality: {}", cli.quality);
     if cli.verbose {
         println!("Verbose mode is ON");
     }
-
     println!("Target files: {:?}", cli.targets);
-    println!("Output type: {}", cli.output);
+    println!("New type: {}", cli.new);
 }
